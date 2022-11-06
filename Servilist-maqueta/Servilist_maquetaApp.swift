a47c825f34075e288663febb1078dc7fcec077ed
Jose Guerra <jose.guerra02@inacapmail.cl>
2022-11-06 18:27:52 -0300

    Adding the project

---
+++ Servilist-maqueta/Servilist_maquetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Servilist_maquetaApp.swift
+//  Servilist-maqueta
+//
+//  Created by José Guerra on 06-11-22.
+//
+
+import SwiftUI
+
+@main
+struct Servilist_maquetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
